ef20fde2724290d58436dad8084f2875440f936a
indigoparadox <indigo.repo@interfinitydynamics.info>
2024-03-24 08:58:06 -0400

    Added scanning and deskewing to PDF script.

--- .scripts/build-pdf.sh
+++ .scripts/build-pdf.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 usage() {
-   echo "$0 [-i input-glob] [-o output-pdf]"
+   echo "$0 [-i input-glob] [-o output-pdf] [-s] [-d duplex] [-c]"
    echo
    echo input-glob - A file or group of input image files to process.
    echo output-pdf - What to call the created PDF.
+   echo -s - Select scanner as input.
+   echo duplex - Can be Front, Back, or Duplex.
+   echo -c - Enabled color scanning.
 }
 
 convert_pdf_ocr() {
+   SCAN_IM_FORMAT="$1"
+   shift
+   PAGES_RESOLUTION="$1"
+   shift
    PDF_OUT="$1"
    shift
    PAGES_GLOB="$@"
@@ -26,10 +33,29 @@ convert_pdf_ocr() {
       IMAGE_BASE="`basename "$i" | sed 's/\.[^.]*$//'`"
       IMAGE_EXT="`basename "$i" | sed 's/.*\.\([^.]*\)/\1/'`"
       echo "processing $IMAGE_BASE..."
-      if [ "png" = "$IMAGE_EXT" ]; then
+      if [ "png" = "$IMAGE_EXT" ] || \
+         [ "pbm" = "$IMAGE_EXT" ] || \
+         [ "tiff" = "$IMAGE_EXT" ]
+      then
          # Convert and add the image to the image list.
          IMAGE_LIST="$IMAGE_LIST $PAGES_TEMP/$IMAGE_BASE.pdf"
-         tesseract -l eng "$i" "$PAGES_TEMP/$IMAGE_BASE" pdf
+
+         echo "deskewing $i..."
+         SKEW_ANGLE="`${IMAGE_EXT}_findskew "$i"`"
+         if [ -n "$SKEW_ANGLE" ]; then
+            echo "skew angle is: $SKEW_ANGLE"
+            SKEW_ANGLE="`echo -1*$SKEW_ANGLE | bc`"
+            echo "deskew angle is: $SKEW_ANGLE"
+            convert "$i" -rotate "$SKEW_ANGLE" \
+               "$PAGES_TEMP/$IMAGE_BASE.$SCAN_IM_FORMAT"
+
+         else
+            # TODO: Just copy to intermediate dir.
+            echo "NOT IMPLEMENTED"
+         fi
+
+         echo "detecting text for $i..."
+         tesseract -l eng "$PAGES_TEMP/$IMAGE_BASE.$SCAN_IM_FORMAT" "$PAGES_TEMP/$IMAGE_BASE" pdf
 
       elif [ "pdf" = "$IMAGE_EXT" ]; then
          # Directly add the input PDF to the image list for appending.
@@ -52,8 +78,34 @@ convert_pdf_ocr() {
    rm -rfv "$PAGES_TEMP"
 }
 
+function scan_input_doc() {
+   SCAN_DUPLEX="$1"
+   SCAN_RESOLUTION="$2"
+   SCAN_MODE="$3"
+   SCAN_FILENAME="$4"
+   SCAN_IM_FORMAT="$5"
+
+   if [ -z "$SCAN_DOC_DEV" ]; then
+      echo "\$SCAN_DOC_DEV not set!"
+      exit 1
+   fi
+
+   if ! /usr/bin/scanimage --batch="$SCAN_FILENAME" --format $SCAN_IM_FORMAT \
+      -d "$SCAN_DOC_DEV" --source "ADF $SCAN_DUPLEX" \
+      --resolution "$SCAN_RESOLUTION" --mode "$SCAN_MODE"
+   then
+      echo "Could the scanner be off?"
+      exit 1
+   fi
+}
+
 PDF_OUTPUT=""
 IMAGE_INPUT=""
+SCAN_INPUT=0
+SCAN_MODE="Lineart"
+SCAN_DUPLEX="Front"
+SCAN_RESOLUTION=300
+SCAN_IM_FORMAT="tiff"
 while [ "$1" ]; do
    case "$1" in
       -i)
@@ -66,6 +118,19 @@ while [ "$1" ]; do
          PDF_OUTPUT="$1"
          ;;
 
+      -s)
+         SCAN_INPUT=1
+         ;;
+
+      -d)
+         shift
+         SCAN_DUPLEX="$1"
+         ;;
+
+      -c)
+         SCAN_MODE="Color"
+         ;;
+
       *)
          echo "extraneous element: $1"
          echo "forgot to single-quote glob?"
@@ -74,8 +139,8 @@ while [ "$1" ]; do
    shift
 done
 
-if [ -z "$IMAGE_INPUT" ]; then
-   echo "no input images specified!"
+if [ $SCAN_INPUT -eq 0 ] && [ -z "$IMAGE_INPUT" ]; then
+   echo "no input images or scanner specified!"
    echo
    usage
    exit 1
@@ -93,5 +158,14 @@ if [ -f "$PDF_OUTPUT" ]; then
    exit 1
 fi
 
-convert_pdf_ocr "$PDF_OUTPUT" "$IMAGE_INPUT"
+if [ $SCAN_INPUT -eq 1 ]; then
+   SCAN_TEMP="`mktemp -d --suffix=.build-pdf`"
+   IMAGE_INPUT="$SCAN_TEMP"'/*'".$SCAN_IM_FORMAT"
+   scan_input_doc "$SCAN_DUPLEX" "$SCAN_RESOLUTION" "$SCAN_MODE" "$SCAN_TEMP/out%d.$SCAN_IM_FORMAT" "$SCAN_IM_FORMAT"
+fi
 
+convert_pdf_ocr "$SCAN_IM_FORMAT" "$SCAN_RESOLUTION" "$PDF_OUTPUT" "$IMAGE_INPUT"
+
+if [ -n "$SCAN_TEMP" ] && [ -d "$SCAN_TEMP" ]; then
+   rm -rvf "$SCAN_TEMP"
+fi
