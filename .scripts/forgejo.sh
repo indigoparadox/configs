1d37bcc26b2bf5d0165071975b027ffb58ea8aeb
indigoparadox <indigo.repo@interfinitydynamics.info>
2025-03-01 12:56:17 -0500

    Started attempting forgejo delete-and-replace...

--- .scripts/forgejo.sh
+++ .scripts/forgejo.sh
@@ -38,15 +38,34 @@ if [ -z "$_forgejo_oauth" ]; then
    exit 1
 fi
 
+_forgejo_pkg_name="`dpkg-deb -I "$_forgejo_target_name" | grep "^ Package" | awk '{print $2}'`"
+_forgejo_pkg_ver="`dpkg-deb -I "$_forgejo_target_name" | grep "^ Version" | awk '{print $2}'`"
+_forgejo_pkg_arch="`dpkg-deb -I "$_forgejo_target_name" | grep "^ Architecture" | awk '{print $2}'`"
+
+if [ -z "$_forgejo_pkg_name" ] || [ -z "$_forgejo_pkg_ver" ] || [ -z "$_forgejo_pkg_arch" ]; then
+   echo "invalid package!"
+   exit 1
+fi
+
 if [ "debian-pkg" = "$_forgejo_action" ]; then
    if [ -z "$_forgejo_pkg_pool" ]; then
       echo "missing package pool!"
       exit 1
    fi
+   echo "uploading $_forgejo_pkg_name-$_forgejo_pkg_ver:$_forgejo_pkg_arch..."
    for pool in $_forgejo_pkg_pool; do
-      curl --user "$FORGEJO_USER:$_forgejo_oauth" \
-         --upload-file "$_forgejo_target_name" \
-         "$FORGEJO_URL/api/packages/$FORGEJO_USER/debian/pool/$pool/main/upload"
+      _forgejo_response="`curl --user "$FORGEJO_USER:$_forgejo_oauth" \
+         --upload-file "$_forgejo_target_name" --no-progress-meter \
+         "$FORGEJO_URL/api/packages/$FORGEJO_USER/debian/pool/$pool/main/upload"`"
+      echo "$_forgejo_response"
+      #if [ "package file already exists" = "$_forgejo_response" ]; then
+      #   echo "attempting to replace..."
+      #   curl --user "$FORGEJO_USER:$_forgejo_oauth" -X DELETE \
+      #      "$FORGEJO_URL/api/packages/$FORGEJO_USER/debian/pools/$pool/main/$_forgejo_pkg_name/$_forgejo_pkg_ver/$_forgejo_pkg_arch"
+      #   curl --user "$FORGEJO_USER:$_forgejo_oauth" \
+      #      --upload-file "$_forgejo_target_name" \
+      #      "$FORGEJO_URL/api/packages/$FORGEJO_USER/debian/pool/$pool/main/upload"
+      #fi
    done
 fi
 
